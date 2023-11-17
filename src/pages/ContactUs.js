3656470ac21f15ec0527967fae5e55d24af85935
Breia Swanigan <33298395+breiaswanigan01@users.noreply.github.com>
2023-11-17 09:32:58 -0500

    updated contact

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -70,41 +70,44 @@ const ContactUs = () => {
           Contact Us
         </h1>
       </div>
-      <div className="m-8 text-xl bg-yellow-200 rounded-3xl p-5">
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="col-span-1">
-              <label className="block">
-                First Name:
+      <div className="m-8 text-lg bg-white rounded-3xl p-5 flex justify-center">
+        <form onSubmit={handleSubmit} className="flex flex-col ">
+          <div className="flex-1  pr-4">
+            <div className="flex flex-col  text-blue-900">
+              <label className="block  ">
+                First Name<span className="text-red-500 pl-1">*</span>
                 <input
                   type="text"
                   placeholder="First Name"
                   name="firstName"
                   value={formData.firstName}
                   onChange={handleChange}
-                  className="m-2 p-2 rounded border border-gray-400 focus:outline-none focus:border-blue-500 w-full"
+                  required
+                  className="m-1 p-1 rounded border border-gray-300 focus:outline-none focus:border-blue-500 w-full"
                 />
               </label>
-              <label className="block">
-                Last Name:
+              <label className="block ">
+                Last Name<span className="text-red-500 pl-1">*</span>
                 <input
                   type="text"
                   placeholder="Last Name"
                   name="lastName"
                   value={formData.lastName}
                   onChange={handleChange}
-                  className="m-2 p-2 rounded border border-gray-400 focus:outline-none focus:border-blue-500 w-full"
+                  required
+                  className="m-2 p-1 rounded border border-gray-300 focus:outline-none focus:border-blue-500 w-full"
                 />
               </label>
               <label className="block">
-                Email:
+                Email<span className="text-red-500 pl-1">*</span>
                 <input
                   type="email"
                   placeholder="Email"
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
-                  className="m-2 p-2 rounded border border-gray-400 focus:outline-none focus:border-blue-500 w-full"
+                  required
+                  className="m-2 p-1 rounded border border-gray-300 focus:outline-none focus:border-blue-500 w-full"
                 />
               </label>
               <label className="block">
@@ -115,55 +118,94 @@ const ContactUs = () => {
                   name="phoneNumber"
                   value={formData.phoneNumber}
                   onChange={handleChange}
-                  className="m-2 p-2 rounded border border-gray-400 focus:outline-none focus:border-blue-500 w-full"
+                  className="m-2 p-1 rounded border border-gray-300 focus:outline-none focus:border-blue-500 w-full"
                 />
               </label>
               <label className="block">
                 Location:
                 <input
-                  type="text"
+                  type="text "
                   placeholder="Location"
                   name="location"
                   value={formData.location}
                   onChange={handleChange}
-                  className="m-2 p-2 rounded border border-gray-400 focus:outline-none focus:border-blue-500 w-full"
+                  className="m-2 p-1 rounded border border-gray-300 focus:outline-none focus:border-blue-500 w-full"
                 />
               </label>
             </div>
             <div className="col-span-1">
-              <label className="block">
+              <label className="block  text-blue-900">
                 Message:
                 <textarea
                   name="message"
                   placeholder="Message"
                   value={formData.message}
                   onChange={handleChange}
-                  className="m-2 p-2 rounded border border-gray-400 focus:outline-none focus:border-blue-500 w-full h-32"
+                  className="m-2 p-1 rounded border border-gray-300 focus:outline-none focus:border-blue-500 w-full h-20 "
                 />
               </label>
               <button
                 type="submit"
-                className="text-white p-2 bg-blue-900 hover:bg-blue-700 rounded-3xl w-full focus:outline-none"
+                className="text-white p-1 bg-blue-900 hover:bg-blue-700 rounded-3xl w-full focus:outline-none"
               >
                 Submit
               </button>
+              <div>
+                {errors.email && <p className="text-red-500">{errors.email}</p>}
+                {errors.firstName && (
+                  <p className="text-red-500">{errors.firstName}</p>
+                )}
+                {errors.lastName && (
+                  <p className="text-red-500">{errors.lastName}</p>
+                )}
+                {/* Add similar error messages for other required fields */}
+              </div>
             </div>
           </div>
         </form>
       </div>
-      <div className="text-4xl grid">
-        <div className="flex ">
-          <FontAwesomeIcon icon={faLocationDot} style={{ color: "#1e3a8a" }} />
-          <p>2785 E. Grand Blvd, Detroit, MI 48211</p>
+      <div className="text-2xl  grid ">
+        <div className="flex  border-b-2">
+          {/* <div className=" flex "> */}
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            style={{ color: "white" }}
+            className="  ml-1 items-center bg-blue-900 rounded-full p-3  "
+          />
+          <div className="pl-5">
+            <p>Address</p>
+            <p>2785 E. Grand Blvd, Detroit, MI 48211</p>
+          </div>
+          {/* </div> */}
         </div>
-        <div className="flex">
-          <FontAwesomeIcon icon={faPhone} style={{ color: "1e3a8a" }} />
-          <p>(248) 862-0663</p>
+        <div className="flex border-b-2">
+          <FontAwesomeIcon
+            icon={faPhone}
+            style={{ color: "white" }}
+            className="  ml-1 items-center bg-blue-900 rounded-full p-3 "
+          />
+          <div className="pl-5">
+            <p>Phone</p>
+            <p>(248) 862-0663</p>
+          </div>
         </div>
-        <div className="flex">
-        <FontAwesomeIcon icon={faEnvelope} style={{ color: "1e3a8a" }} />
-
-       <a href="mailto:jayriderllc@gmail.com" target="_blank" rel="noopener noreferrer"> Email Us </a>
+        <div className=" flex">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            style={{ color: "white" }}
+            className=" ml-1 items-center bg-blue-900 rounded-full p-3"
+          />
+          <div className="pl-5">
+            <p>Email</p>
+            <a
+              href="mailto:jayriderllc@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Email Us{" "}
+            </a>
+          </div>
         </div>
       </div>
     </>
